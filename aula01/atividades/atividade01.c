1dc57ea3ef4c91825528fdf5d8a6493b65f7fd9c
Victoria Trois <vickytrois@outlook.com>
2026-02-28 20:22:50 +0000

    realiza atividade01 da aula01

--- aula01/atividades/atividade01.c
+++ aula01/atividades/atividade01.c
@@ -1,1 +1,24 @@
-// Considerando o arquivo cidades.txt, formado por três colunas (Nome da cidade, População e UF), gere um programa que leia o arquivo e mostre o conteúdo na tela.
+// Considerando o arquivo cidades.txt, formado por três colunas (Nome da cidade, População e UF),
+// gere um programa que leia o arquivo e mostre o conteúdo na tela.
+
+#include <stdio.h>
+
+int main() {
+  char nomeDaCidade[12];
+  unsigned int populacao;
+  char sigla[3];
+
+  FILE *cidades;
+
+  if ((cidades = fopen("cidades.txt", "r")) == NULL) {
+    printf("Erro ao abrir o arquivo");
+    return 1;
+
+  } else {
+    while (!feof(cidades)) {
+      fscanf(cidades, "%s %u %s", nomeDaCidade, &populacao, sigla);
+      printf("%s %u %s\n", nomeDaCidade, populacao, sigla);
+    }
+    return 0;
+  }
+}
