c99d28e44d4b2a80844286415031241c865e39f0
Victoria Trois <vickytrois@outlook.com>
2026-02-28 17:08:40 +0000

    revisa exemplo01 dado em aula

--- aula01/exemplos/exemplo01_desafio.c
+++ aula01/exemplos/exemplo01_desafio.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main()
+int main()
 {
     int codigo;
     char empresa[10];
     char sobre[12];
-    char nome[10];
-    char funcao[10];
+    // Tem alguns nomes e funções que precisam de mais do que 13 bytes para serem armazenadas
+    char nome[12];
+    // char funcao[10];
+    // faz o buffer estourar e imprimir io em lugares que não deveria
+    char funcao[20];
 
     FILE *txt;
 
@@ -20,8 +23,8 @@ main()
     else
         {
             while (!feof(txt)) {
-                fscanf(txt, "%s %s %s %s %d", &empresa, sobre, nome, funcao, &codigo);
-                printf("%-10s %-12s %-10s %-10s \t %d\n", empresa, sobre, nome, funcao, codigo);
+                fscanf(txt, "%d %s %s %s %s ", &codigo, empresa, sobre, nome, funcao);
+                printf("%-10s %-12s %-10s %-13s %d\n", empresa, sobre, nome, funcao, codigo);
                 }
             fclose(txt);
         }
