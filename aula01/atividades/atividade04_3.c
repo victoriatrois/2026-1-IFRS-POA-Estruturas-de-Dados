61fe9c5263a6d7a21fdd4661d1ed3df296d4bb5c
Victoria Trois <vickytrois@outlook.com>
2026-03-01 02:36:29 +0000

    realiza atividade 04.3

--- aula01/atividades/atividade04_3.c
+++ aula01/atividades/atividade04_3.c
@@ -1,3 +1,27 @@
-// 1) Considerando o Exemplo03.cpp, altere o programa que nodo de leia dez (10) nomes de pessoas e suas respectivas idades e grave em um arquivo chamado lista.txt. 
-// 2) Considerando o Exemplo04.cpp, altere o Programa que modo que ao mostrar o conteúdo do Arquivo, coloque todas as vogais em Maiúsculas na tela e em uma novo arquivo, chamado Texto_Alterado.txt. 
-// 3) Considerando o Exemplo05.cpp, altere o programa que modo que para cada parágrafo ao arquivo seja impresso uma linha em branco. 
+// 3) Considerando o Exemplo05.cpp,
+// altere o programa de que modo que, para cada parágrafo, ao arquivo ser impresso,
+//   seja adicionada uma linha em branco
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char linha[1024]; 
+
+  FILE *txt = fopen("arquivo_texto.txt","r");
+
+  if(txt == NULL) {
+    printf("Erro ao abrir arquivo");
+  }
+
+  while (!feof(txt)) {
+    fgets(linha, 1024, txt);
+    printf("%s", linha);
+    
+    if(linha[0] == '\n' || (linha[0] == '\r' && linha[1] == '\n')) {
+      printf("\n");
+    }
+  }
+
+  fclose(txt);
+}
