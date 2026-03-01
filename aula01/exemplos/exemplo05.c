11556bf5ea5744c4a288a0c1b45e64088d3364d3
Victoria Trois <vickytrois@outlook.com>
2026-03-01 02:15:50 +0000

    refactor: improve style

--- aula01/exemplos/exemplo05.c
+++ aula01/exemplos/exemplo05.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main()
-{
-char linha[1024]; 
+main() {
+  char linha[1024]; 
 
-FILE *txt;
+  FILE *txt = fopen("arquivo_texto.txt","r");
 
-if((txt = fopen("arquivo_texto.txt","r")) == NULL)
-    {
-        printf("Erro ao abrir arquivo");
-    }
- else
-     {
-        while (!feof(txt)) {
-               fgets(linha, 1024, txt);
-               printf("%s", linha);
-               system("pause");
-              }
-          fclose(txt);
-      }
-system("pause"); 
+  if(txt == NULL) {
+    printf("Erro ao abrir arquivo");
+  }
+
+  while (!feof(txt)) {
+    fgets(linha, 1024, txt);
+    printf("%s", linha);
+    system("pause");
+  }
+  fclose(txt);
+  system("pause"); 
 }
-
-
