d355a419ffd13907fdde0010ddcc4f5ec2d3a677
Victoria Trois <vickytrois@outlook.com>
2026-03-01 02:35:34 +0000

    realiza atividade 04.2

--- aula01/atividades/atividade04_2.c
+++ aula01/atividades/atividade04_2.c
@@ -1,1 +1,44 @@
-// 2) Considerando o Exemplo04.cpp, altere o Programa que modo que ao mostrar o conteúdo do Arquivo, coloque todas as vogais em Maiúsculas na tela e em uma novo arquivo, chamado Texto_Alterado.txt.
+// 2) Considerando o Exemplo04.cpp,
+// altere o Programa que modo que ao mostrar o conteúdo do arquivo,
+//    todas as vogais tornem-se maiúsculas na tela e em uma novo arquivo, chamado Texto_Alterado.txt.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char caractere; 
+  FILE *arquivo = fopen("arquivo.txt","r");
+  FILE *texto_alterado = fopen("Texto_Alterado.txt","w");
+
+  if (arquivo == NULL || texto_alterado == NULL) {
+    printf("Erro ao abrir arquivo");
+
+    return 1;
+  }
+
+  while ((caractere = fgetc(arquivo)) != EOF) {
+    switch (caractere) {
+      case 'a':
+        caractere = 'A';
+        break;
+      case 'e':
+        caractere = 'E';
+        break;
+      case 'i':
+        caractere = 'I';
+        break;
+      case 'o':
+        caractere = 'O';
+        break;
+      case 'u':
+        caractere = 'U';
+        break;
+    }
+
+    fprintf(texto_alterado,"%c", caractere);
+  }
+
+  fclose(arquivo);
+  fclose(texto_alterado);
+  return 0;
+}
